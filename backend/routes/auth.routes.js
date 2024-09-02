273ca2fca2d05fb9e1d9dcb5ae188409b2c2ca6b
Vanraj8169 <truecaptures@gmail.com>
2024-09-02 11:37:04 +0530

    Initialized frontend

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
+router.post("/signup", (req, res) => {
   res.json({ data: "You hit signup route" });
 });
 export default router;
