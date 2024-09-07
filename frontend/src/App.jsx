6e8afd12794a8f018b43412661846d2b17cba38b
Vanraj Pardeshi <truecapturers@gmail.com>
2024-09-08 01:05:27 +0530

    Added Homepage | Login | Signup page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/Home/HomePage";
+import SignUpPage from "./pages/auth/signup/SignUp";
+import LoginPage from "./pages/auth/login/LoginPage";
+import Sidebar from "./components/common/SideBar";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
   );
 }
 
