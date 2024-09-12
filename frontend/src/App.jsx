2557f4d5e348d43349ef3285c8e746a18c6441c3
Vanraj Pardeshi <truecapturers@gmail.com>
2024-09-12 17:33:17 +0530

    Updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import SignUpPage from "./pages/auth/signup/SignUp";
 import LoginPage from "./pages/auth/login/LoginPage";
 import Sidebar from "./components/common/SideBar";
 import RightPanel from "./components/common/RightPanel";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/Profile/ProfilePage";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
       </Routes>
       <RightPanel />
     </div>
