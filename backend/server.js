d501e259e04c6f9281dc88b9fc8725c8be6708ba
Vanraj Pardeshi <truecapturers@gmail.com>
2024-09-21 17:06:58 +0530

    create post connected with backend api url

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 // middlewares
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
