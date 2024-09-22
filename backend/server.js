d86c45eb32695925f969ad2f9e5cd5adb207d435
Vanraj Pardeshi <truecapturers@gmail.com>
2024-09-23 00:51:15 +0530

    Fixed CORS error

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { v2 as cloudinary } from "cloudinary";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
